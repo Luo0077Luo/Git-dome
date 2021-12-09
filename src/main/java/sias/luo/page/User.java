964dc92b713c2e008cd24b7bbcfe2b54f6cc97da
luo <2519007677@qq.com>
2021-12-09 16:23:47 +0800

    修改3.1

--- src/main/java/sias/luo/page/User.java
+++ src/main/java/sias/luo/page/User.java
@@ -8,5 +8,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("陈先生  ，你好，我是你爸爸");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
