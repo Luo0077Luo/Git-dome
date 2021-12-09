58b81683941e90a5ea916b55957e9e16b26a6b20
luo <2519007677@qq.com>
2021-12-09 16:20:12 +0800

    第一次提交1.1

---
+++ src/main/java/sias/luo/page/User.java
@@ -0,0 +1,11 @@
+package sias.luo.page;
+
+/**
+ * @author shkstart
+ * @create 2021-12-09-16:15
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("陈先生  ，你好，我是你爸爸");
+    }
+}
